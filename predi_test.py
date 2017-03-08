0e09f9734978cbd4671159db6b3a6ab660b302c0
Nobody <no@example.net>
2017-03-08 06:15:18 -0800

    Start a real unittest.

--- predi_test.py
+++ predi_test.py
@@ -1,17 +1,34 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+import unittest
 import predi
 
-m = predi.Market('The ponies will pwn!', 0.5)
-m.Bid('Amy', 0.4)
-m.Bid('Bob', 0.1)
-m.Bid('Amy', 0.8)
-m.Bid('Cathy', 0.7)
-m.Bid('Bob', 0.8)
-m.Bid('Drew', 0.9)
-m.Resolve(True)
+class PrediTest(unittest.TestCase):
+    def testMarket(self):
+        m = predi.Market('The ponies will pwn!', 0.5)
+        m.Bid('Amy', 0.4)
+        m.Bid('Bob', 0.1)
+        m.Bid('Amy', 0.8)
+        m.Bid('Cathy', 0.7)
+        m.Bid('Bob', 0.8)
+        m.Bid('Drew', 0.9)
+        m.Resolve(True)
 
-for player in m.results:
-    print "{} {:.1f}".format(player, m.results[player])
+        second = lambda x: x[1]
+        rank = list(m.results.items())
+        rank.sort(key=second, reverse=True)
+        for player, score in rank:
+            print("{} : {} centibits".format(player, score))
+
+        # Check against the teacher's password.
+        book = {'Amy': 300 - 32,
+                'Bob': -200 + 19,
+                'Cathy': -19,
+                'Drew': 17}
+        for player, score in rank:
+            diff = book[player] - score
+            self.assertLess(diff, 1, "{} diff too big {}".format(player, diff))
 
 
+if __name__ == '__main__':
+    unittest.main()
