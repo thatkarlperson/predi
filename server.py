60c62abc0e1ccd922cd93f7f0b9eaa785f043e83
Nobody <no@example.net>
2017-03-16 08:17:41 -0700

    Support showing many markets (not entirely working)

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 from predi import Market
+
 m = Market('The ponies will pwn!', 0.5)
 m.Bid('Amy', 0.4)
 m.Bid('Bob', 0.1)
@@ -11,17 +12,24 @@ m.Bid('Amy', 0.8)
 m.Bid('Cathy', 0.7)
 m.Bid('Bob', 0.8)
 m.Bid('Drew', 0.9)
-# m.Resolve(True)
+m.Resolve(True)
+
+n = Market("The cats will cat!", 0.7)
+n.Bid('Cathy', 0.5)
+n.Bid('Bob', 0.6)
+n.Bid('Amy', 0.3)
+
+markets = [m, n]
 
 
 @app.route('/')
-def testMarket():
-    return render_template('market.html', market=m)
+def redirMain():
+    return redirect(url_for("listAll"))
 
 
 @app.route('/markets', methods=['GET'])
 def listAll():
-    return 'nope'
+    return render_template('allmarkets.html', count=len(markets))
 
 
 @app.route('/markets', methods=['POST'])
@@ -31,7 +39,7 @@ def createMarket():
 
 @app.route('/market/<int:market_id>', methods=['GET'])
 def showMarket(market_id):
-    return 'nope'
+    return render_template('market.html', market=markets[market_id])
 
 
 @app.route('/market/<int:market_id>', methods=['POST'])
