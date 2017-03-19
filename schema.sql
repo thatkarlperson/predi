999149796f763e08428188ec43fe9e8f3e57a5ac
Nobody <no@example.net>
2017-03-19 15:20:31 -0700

    Fix schema typos.

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ create table markets (
 	title text,
 	close_time timestamptz,
 	resolve_time timestamptz,
-	open_time timestamptz default now,
+	open_time timestamptz default now(),
 	state market_state,
 	id serial primary key
 );
@@ -18,10 +18,10 @@ create table players (
 );
 
 create table bids (
-	market references markets.id,
-	player references players.id,
-	value decimal(2,2) constraint (value > 0 and value < 1),
-	bid_time timestamptz default now,
+	market integer references markets (id),
+	player integer references players (id),
+	value decimal(2,2) check (value > 0 and value < 1),
+	bid_time timestamptz default now(),
 	id serial
 );
 
